6de4085a11ee613166f40ee41364d29831ea0446
Robert <51803593+rotyvk7@users.noreply.github.com>
2022-02-14 16:23:24 -0300

    Exercício009
    Registrando ações com os botões, a cada clique, vai aparecer na tela qual botão foi acionado.

---
+++ Exercícios/ex009/acoes.js
@@ -0,0 +1,18 @@
+
+let acionar = document.getElementById('res')
+
+function text1(){
+    acionar.innerHTML += '<p>Clicou no primeiro botão</p>'
+}
+
+function text2(){
+    acionar.innerHTML += '<p>Clicou no segundo botão</p>'
+}
+
+function text3(){
+    acionar.innerHTML += '<p>Clicou no terceiro botão</p>'
+}
+
+function text4(){
+    acionar.innerHTML += '<p>Clicou no quarto botão</p>'
+}
