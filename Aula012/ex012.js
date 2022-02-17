09f6db911856880b50e5acf112ed50a6c1719c84
Robert <51803593+rotyvk7@users.noreply.github.com>
2022-02-16 21:34:23 -0300

    Exercícios

---
+++ Aula012/ex012.js
@@ -0,0 +1,10 @@
+ var idade = 68
+ if(idade < 16){
+     console.log(`Não vota`)
+ } else {
+     if(idade < 18 || idade > 67) {
+     console.log(`Voto Opcional`)
+    } else {
+        console.log(`Voto obrigatório`)
+    }
+ }  
---
+++ aula12ex/Modelo/script.js
@@ -0,0 +1,29 @@
+function carregar(){
+    var msg = document.getElementById('msg')
+    img = document.getElementById('img')
+    var agora = new Date()
+    var hora = agora.getHours()
+    msg.innerHTML = `Agora são ${hora} horas`
+
+    if(hora >= 5 && hora <= 12){
+        //BOM DIA!
+        img.src="manha.jpg"
+        msg.innerHTML += '<p>Bom dia!</p>'
+        document.body.style.background = '#1A8FE2'
+    } else if(hora <= 18 && hora > 12) {
+        //BOA TARDE!
+        img.src="tarde.jpg"
+        msg.innerHTML += '<p>Boa tarde!</p>'
+        document.body.style.background = '#F99728'
+    } else if(hora > 18 && hora <= 23){
+        //BOA NOITE!
+        img.src="noite.jpg"
+        msg.innerHTML += '<p>Boa noite!</p>'
+        document.body.style.background = '#9E79AE'
+    } else {
+        img.src="madrugada.jpg"
+        msg.innerHTML += '<p>Boa madrugada!</p>'
+        document.body.style.background = '#1A2C33'
+        //BOA MADRUGADA!
+    }
+}
