41eefbd1f071488eaa5affd1a15ccd2593c7a43c
Robert <51803593+rotyvk7@users.noreply.github.com>
2022-03-10 19:29:30 -0300

    Create ambiente.js

---
+++ Aula13/ambiente.js
@@ -0,0 +1,25 @@
+/*
+console.log('comer fatia')
+console.log('comer fatia')
+console.log('comer fatia')
+console.log('comer fatia')
+console.log('comer fatia')
+console.log('comer fatia')
+*/
+
+/*var c = 1
+
+while (c <= 500){
+    console.log(`fatia ${c}`)
+    c++
+}*/
+
+var c = 1
+
+do {
+    console.log(`fatia ${c}`)
+    c++
+}
+while (c <= 500){
+
+}
