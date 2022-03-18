1c2f9edefd90f9b1ba3853d6af429822105a07ba
Robert <51803593+rotyvk7@users.noreply.github.com>
2022-03-17 23:11:47 -0300

    exercicios

---
+++ aula14ex/ex016/script.js
@@ -0,0 +1,19 @@
+function verificar(){
+
+    let inicio = document.getElementById('txtinicio')
+    let fim = document.getElementById('txtfim')
+    let passo = document.getElementById('txtpasso')
+    let res = document.getElementById('res')
+
+    if(inicio.value.length == 0 || fim.value.length == 0 || passo.value.length ==0 ) {
+        window.alert ('[ERRO] faltam dados!')
+    } else {
+        res.innerHTML = 'Contando:  '
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+
+        for (let c = i; c <= f; c += p)
+        res.innerHTML += `${c} `
+    }
+}
